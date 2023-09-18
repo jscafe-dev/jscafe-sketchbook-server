52fc4b3622c36e0bd6bf09271f525d792caa6e6d
Vedant Jain <jscafe.business@gmail.com>
2023-09-18 17:43:25 +0530

    emit and receive events

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ const io = new Server(httpServer, { cors: 'http://localhost:3000' });
 
 io.on("connection", (socket) => {
   console.log("server connected")
+
+  socket.on('beginPath', (arg) => {
+    socket.broadcast.emit('beginPath', arg)
+  })
+
+  socket.on('drawLine', (arg) => {
+    socket.broadcast.emit('drawLine', arg)
+  })
+
+  socket.on('changeConfig', (arg) => {
+    socket.broadcast.emit('changeConfig', arg)
+  })
 });
 
 httpServer.listen(5000);
