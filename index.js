da8cdeb17fb4ebc9af8204cd6d5669140f4705e6
Vedant Jain <jscafe.business@gmail.com>
2023-09-18 17:56:28 +0530

    whitelist client

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const { Server } = require("socket.io");
 const cors = require('cors');
 
 const app = express();
-app.use(cors({origin: 'http://localhost:3000'}))
+const isDev = app.settings.env === 'development'
+const URL = isDev ? 'http://localhost:3000' : 'https://sketchbook-sigma.vercel.app'
+app.use(cors({origin: URL}))
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: 'http://localhost:3000' });
+const io = new Server(httpServer, { cors: URL });
 
 io.on("connection", (socket) => {
   console.log("server connected")
